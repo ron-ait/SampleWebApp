bceba1f1c2ecac5bfb6ad6621bbcd2fa2121414a
ron-ait <100356208+ron-ait@users.noreply.github.com>
2022-04-19 14:53:44 +0530

    Update TestCalculator.java

--- src/example/junit/TestCalculator.java
+++ src/example/junit/TestCalculator.java
@@ -1,9 +1,9 @@
 package junit;
 
 
-import org.junit.Assert.assertEquals;
-import org.junit.Assert.assertFalse;
-import org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
